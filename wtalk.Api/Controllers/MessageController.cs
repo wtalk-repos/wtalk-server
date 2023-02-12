c051e786c960f4e19cf52d74007c04f343737500
Bojan Balinovic <bojanbalinovic1@gmail.com>
2023-02-12 14:27:32 +0100

    feat: query messages

--- wtalk.Api/Controllers/MessageController.cs
+++ wtalk.Api/Controllers/MessageController.cs
@@ -5,14 +5,16 @@ using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using System.Threading.Tasks;
 using wtalk.Cqrs.Commands.Message;
+using wtalk.Cqrs.Queries.Message;
 using Wtalk.Core.Responses.Message;
+using Wtalk.Core.Specifications.Message;
 
 namespace wtalk.Controllers
 {
     [Route("api/message")]
     [ApiController]
     [Authorize]
-    public class MessageController:ControllerBase
+    public class MessageController : ControllerBase
     {
         private readonly IMediator _mediator;
 
@@ -29,6 +31,18 @@ namespace wtalk.Controllers
         public async Task<ActionResult<CreateMessageResponse>> SendAndPersistMessageCommand(SendAndPersistMessageCommand request)
         {
             var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+        [HttpGet("list")]
+        [SwaggerOperation(Summary = "Get message list.", Description = "Get message list.")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<GetMessageListResponse>> GetMessageList([FromQuery] MessageSpecParams specParams)
+        {
+            var response = await _mediator.Send(new GetMessageListQuery { MessageSpecParams = specParams });
             return Ok(response);
         }
     }
