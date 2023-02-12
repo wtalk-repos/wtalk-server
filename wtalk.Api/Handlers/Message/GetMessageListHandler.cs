e566b5f126d423c6d2b205b9d9bdcb9ec48b43dc
Bojan Balinovic <bojanbalinovic1@gmail.com>
2023-02-12 14:25:38 +0100

    feat: query messages

---
+++ wtalk.Api/Handlers/Message/GetMessageListHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using wtalk.Cqrs.Queries.Message;
+using Wtalk.Core.Helpers;
+using Wtalk.Core.Interfaces.Repositories;
+using Wtalk.Core.Responses.Message;
+using Wtalk.Core.Specifications.Message;
+
+namespace wtalk.Handlers.Message
+{
+    public class GetMessageListHandler : IRequestHandler<GetMessageListQuery, GetMessageListResponse>
+    {
+        private readonly IReadGenericRepository<Wtalk.Core.Entities.Message> _readGenericRepository;
+        private readonly IMapper _mapper;
+        public GetMessageListHandler(
+            IReadGenericRepository<Wtalk.Core.Entities.Message> readGenericRepository, IMapper mapper)
+        {
+            _readGenericRepository = readGenericRepository;
+            _mapper = mapper;
+        }
+        public async Task<GetMessageListResponse> Handle(GetMessageListQuery request, CancellationToken cancellationToken)
+        {
+            var spec = new MessageSpecification(request.MessageSpecParams);
+            var messages = await _readGenericRepository.ListAsync(spec);
+            var totalItems = await _readGenericRepository.CountAsync(spec);
+            var messageResponses = _mapper.Map<IReadOnlyList<MessageResponse>>(messages);
+
+            return new GetMessageListResponse
+            {
+                Pagination = new Pagination<MessageResponse>(request.MessageSpecParams.PageIndex, request.MessageSpecParams.PageSize, totalItems, messageResponses)
+            };
+        }
+    }
+}
---
+++ wtalk.Core/Specifications/Message/MessageSpecification.cs
@@ -0,0 +1,21 @@
+﻿namespace Wtalk.Core.Specifications.Message
+{
+    public class MessageSpecification : BaseSpecification<Core.Entities.Message>
+    {
+        public MessageSpecification(MessageSpecParams messageSpecParams)
+        {
+            if (!string.IsNullOrEmpty(messageSpecParams.Sort))
+            {
+                switch (messageSpecParams.Sort)
+                {
+                    case "timestampDesc":
+                        AddOrderByDescending(e => e.Timestamp);
+                    break;
+                }
+            }
+
+            ApplyPaging(messageSpecParams.PageSize * (messageSpecParams.PageIndex - 1), messageSpecParams.PageSize);
+
+        }
+    }
+}
